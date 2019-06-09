5b33d4bd6914dbe6b8b9311dc9f029b787576651
Minh <minhta16@augustana.edu>
2019-06-09 18:13:48 +0700

    Add HideOnScroll to hide appbar when scrolling

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,17 @@
 /* eslint-disable no-undef */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Adapter from 'enzyme-adapter-react-16';
+import { configure, shallow, mount } from 'enzyme';
+import toJson from 'enzyme-to-json';
+import App from 'App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+configure({ adapter: new Adapter() });
+
+describe('<App /> shallow rendering', () => {
+  it('render App without crashing', () => {
+    // const wrapper = shallow(<App />);
+    expect(1 + 2).toBe(3);
+    console.log(wrapper);
+  });
 });
